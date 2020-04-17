03153f0a1a4c165a30b795b9b5dad25f4934a30c
mumu <617958070@qq.com>
2020-04-18 00:46:36 +0800

    自定义item组件

---
+++ cmps/movie-item/movie-item.js
@@ -0,0 +1,34 @@
+// cmps/movie-item/movie-item.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        name: {
+            type: String,
+            value: ''
+        },
+        size: {
+            type: Number,
+            value: 0
+        },
+        movie: {
+            type: Object,
+            value: {}
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
